8d76e7894817930f6c7bf323d1a8f26a4a316997
Taha Moeini <49568082+tahamoeini@users.noreply.github.com>
2026-09-06 21:39:49 +0330

    Add package API

---
+++ src/pdf_sanitizer/__init__.py
@@ -0,0 +1,5 @@
+from .config import ExtractionConfig
+from .extractor import ExtractionResult, extract_pdf
+
+__all__ = ["ExtractionConfig", "ExtractionResult", "extract_pdf"]
+__version__ = "0.1.0"
